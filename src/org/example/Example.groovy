3c59a9071ab4e2a9b64fd08017a11905223f8605
adrianstawarz98 <adrianstawarz98@gmail.com>
2023-03-01 22:24:32 +0100

    Add sample files

---
+++ src/org/example/Example.groovy
@@ -0,0 +1,7 @@
+package src.org.example
+
+class Example {
+    static void sayHello() {
+        echo "HELLO FROM EXAMPLE CLASS"
+    }
+}
---
+++ vars/example.groovy
@@ -0,0 +1,7 @@
+package vars
+import src.org.example.Example
+
+def call() {
+    Example.sayHello()
+    echo "SAMPLE TEXT"
+}
